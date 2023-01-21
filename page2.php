08937a34bdb6c481badf25117bc4f3456f8b35ca
adinda0 <87028157+adinda0@users.noreply.github.com>
2023-01-21 09:46:46 +0700

    Add files via upload

---
+++ page2.php
@@ -0,0 +1,35 @@
+<CENTER>
+<table border="1px" width="70%" cellpadding="5" cellspacing="0">
+<tr>
+	<th>No :</th>
+	<th>Nama :</th>
+	<th>Jabatan</th>
+	<th>NO Hp:</th>
+</tr>
+<tr>
+	<td>1.</td>
+	<td>ADINDA TRISISTA</td>
+	<td>DIREKTUR UTAMA</td>
+	<td>085210792281</td>
+</tr>
+<tr>
+	<td>2.</td>
+	<td>RYO PERDINAN</td>
+	<td>WAKIL DIREKTUR</td>
+	<td>089837263827</td>
+</tr>
+<tr>
+	<td>3.</td>
+	<td>MELISA S</td>
+	<td>SEKRETARIS</td>
+	<td>087674287678</td>
+</tr>
+<tr>
+	<td>3.</td>
+	<td>CITA LOLANDIA</td>
+	<td>BENDAHARA</td>
+	<td>0858598989</td>
+</tr>
+
+ </table>
+</CENTER>
